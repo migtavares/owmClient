8ca4d9b9718ae68a1fdc4bae8efff4ad844c2092
J. Miguel P. Tavares <migtavares@gmail.com>
2013-01-06 20:21:59 +0100

    Initial project setup.

---
+++ lib/src/main/java/org/bitpipeline/lib/owm/OwmClient.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright 2012 
+ *         J. Miguel P. Tavares <mtavares@bitpipeline.eu>
+ *         BitPipeline
+ */
+package org.bitpipeline.lib.owm;
+
+
+/** Implements a synchronous HTTP client to the Open Weather Map service described
+ * in http://openweathermap.org/wiki/API/JSON_API
+ * @author mtavares */
+public class OwmClient {
+
+	/** Find current weather in a city */
+	public String findCurrentByCityName (String string) {
+		return null;
+	}
+}
---
+++ lib/src/test/java/org/bitpipeline/lib/owm/CurrentWeatherTest.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright 2012 
+ *         J. Miguel P. Tavares <mtavares@bitpipeline.eu>
+ *         BitPipeline
+ */
+package org.bitpipeline.lib.owm;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * 
+ * @author mtavares */
+public class CurrentWeatherTest {
+
+	/**
+	 * @throws java.lang.Exception */
+	@Before
+	public void setUp () throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception */
+	@After
+	public void tearDown () throws Exception {
+	}
+
+	@Test
+	public void testWeatherByCityName () {
+		OwmClient owm = new OwmClient ();
+		String answer = owm.findCurrentByCityName ("Lisbon");
+		System.out.println (answer);
+	}
+
+}
