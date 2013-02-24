78cd77af452bb88c3800b7410352af564240a52c
J. Miguel P. Tavares <migtavares@gmail.com>
2013-02-24 10:57:37 +0100

    Parse the distance to the requested coordinates.

--- lib/src/main/java/org/bitpipeline/lib/owm/LocalizedWeatherData.java
+++ lib/src/main/java/org/bitpipeline/lib/owm/LocalizedWeatherData.java
@@ -18,8 +18,9 @@ package org.bitpipeline.lib.owm;
 import org.json.JSONObject;
 
 public class LocalizedWeatherData extends WeatherData {
-	private static final String JSON_URL   = "url";
-	private static final String JSON_COORD = "coord";
+	private static final String JSON_URL      = "url";
+	private static final String JSON_COORD    = "coord";
+	private static final String JSON_DISTANCE = "distance";
 
 	public static class GeoCoord {
 		private static final String JSON_LAT = "lat";
@@ -50,11 +51,13 @@ public class LocalizedWeatherData extends WeatherData {
 
 	private final String url;
 	private final GeoCoord coord ;
+	private final float distance;
 
 	public LocalizedWeatherData (JSONObject json) {
 		super (json);
 
 		this.url = json.optString (LocalizedWeatherData.JSON_URL);
+		this.distance = (float) json.optDouble (LocalizedWeatherData.JSON_DISTANCE, Double.NaN);
 
 		JSONObject jsonCoord = json.optJSONObject (LocalizedWeatherData.JSON_COORD);
 		this.coord = jsonCoord != null ? new GeoCoord (jsonCoord) : null;
@@ -72,5 +75,12 @@ public class LocalizedWeatherData extends WeatherData {
 	}
 	public GeoCoord getCoord () {
 		return this.coord;
+	}
+
+	public boolean hasDistance () {
+		return !Float.isNaN (this.distance);
+	}
+	public float getDistance () {
+		return this.distance;
 	}
 }
