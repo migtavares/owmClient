d0685ede27e219c7af1c01fa9c46081f9a83a550
J. Miguel P. Tavares <migtavares@gmail.com>
2016-02-08 22:56:14 +0100

    Remove unused import.

--- lib/src/test/java/org/bitpipeline/lib/owm/ForecastWeatherDataTest.java
+++ lib/src/test/java/org/bitpipeline/lib/owm/ForecastWeatherDataTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.*;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
